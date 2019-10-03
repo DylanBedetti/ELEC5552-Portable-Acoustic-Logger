0d114f3577bd0e3fb5029e28180216d8903900f2
Bradley Willams <williamsabrad@Bradleys-MacBook-Pro.local>
2019-10-03 12:29:01 +0800

    MEMS I2s MICs printing to serial port

--- AnalogueMIC/AnalogueMIC.ino
+++ AnalogueMIC/AnalogueMIC.ino
@@ -6,22 +6,30 @@ int cal=0;
 #include <SD.h>
 #include <SPI.h>
 
-#define SD_ChipSelectPin 9
-#define buffSize 128
-#define ENABLE_RECORDING
-TMRpcm audio;
-int audiofile = 0;
-unsigned long i = 0;
-bool recmode = 0;
-char filename[]="1.WAV";
-long mic_loop_timer;
-int count;
-int auCount;
+#define SD_ChipSelectPin 8
+
+TMRpcm audio1;
+TMRpcm audio2;
+TMRpcm audio3;
+
+
+char filenameA[]="A1.WAV"; //Audio Input file 1
+char filenameB[]="B1.WAV"; //Audio Input file 2
+char filenameC[]="C1.WAV"; //Audio Inout file 3
+long mic_loop_timer; //loop timer
+
+int count;//generic counter
+int auCount; //integer to itterate audio files
+
+
 void setup(){
   Serial.begin(9600);
-  pinMode(A0, INPUT);
-  pinMode(6, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
+  pinMode(A0, INPUT_PULLUP); //setting audio analoginput A
+  pinMode(A1, INPUT_PULLUP); //setting audio analog input B
+  pinMode(A2, INPUT_PULLUP); //setting audio analog input C
+
+  // Setting Up SD CARD
+  
   if (!SD.begin(SD_ChipSelectPin)) {
      delay(4000);
     Serial.println("Card failed, or not present");
@@ -30,26 +38,39 @@ void setup(){
   }  
   delay(4000);
   Serial.println("card initialized.");
-  audio.CSPin = SD_ChipSelectPin;
+  audio1.CSPin, audio2.CSPin, audio2.CSPin = SD_ChipSelectPin;//setting audio object select pins
+  
   Serial.println("Finished Calibrating");
-
+  int sum;
+  
 }
 
 void loop(){
-  
+                                 
    if(mic_loop_timer==0|millis()-mic_loop_timer>=15000){//create a new wave file every 1/4 minutes
-    if(count!=0){
+    if(count!=0){ 
      Serial.println("Recording Stopped: ");
-     Serial.println(filename);
-     audio.stopRecording(filename); 
+     Serial.println(filenameA);
+     audio1.stopRecording(filenameA); 
+     Serial.println(filenameB);
+     audio2.stopRecording(filenameB);
+     Serial.println(filenameC);
+     audio3.stopRecording(filenameC); 
     }
-    delay(100000);
-    count=1;
-    auCount++;
-    filename[0]=auCount+'0';
+    delay(50000);
+    count=1; // make sure you start stopping files
+    auCount++;//make sure you increase file number
+    filenameA[1]=auCount+'0';
+    filenameB[1]=auCount+'0';
+    filenameC[1]=auCount+'0';
+    
     Serial.println("Recording Started: ");
-    Serial.println(filename); 
-    audio.startRecording(filename, 40000, A0); 
+    Serial.println(filenameA); 
+    audio1.startRecording(filenameA, 20000, A0); 
+    Serial.println(filenameB); 
+    audio2.startRecording(filenameB, 20000, A1);
+    Serial.println(filenameC); 
+    audio3.startRecording(filenameB, 20000, A2);
     mic_loop_timer=millis();
   }
 }
---
+++ Microphones/Microphones.ino
@@ -0,0 +1,39 @@
+#include <SD.h>
+#include <ArduinoSound.h>
+
+AmplitudeAnalyzer amp;
+
+void setup(){
+// A baud rate of 115200 is used instead of 9600 for a faster data rate
+  // on non-native USB ports
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // setup the I2S audio input for 44.1 kHz with 32-bits per sample
+  if (!AudioInI2S.begin(44100, 32)) {
+    Serial.println("Failed to initialize I2S input!");
+    while (1); // do nothing
+  }
+
+  // configure the I2S input as the input for the amplitude analyzer
+  if (!amp.input(AudioInI2S)) {
+    Serial.println("Failed to set amplitude analyzer input!");
+    while (1); // do nothing
+  }
+}
+
+void loop(){
+
+ // check if a new analysis is available
+  if (amp.available()) {
+    // read the new amplitude
+    int in = amp.read();
+
+    // print out the amplititude to the serial monitor
+    Serial.println(in>>16);
+    
+  }
+  
+}
